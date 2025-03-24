b8e3ec3c2970ef33459c7b501c2895403dfde36d
Paulo Henrique Nalcino <phnalcino@gmail.com>
2025-03-23 22:40:29 -0300

    home: update background image

--- klyra/src/App.jsx
+++ klyra/src/App.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./components/navbar/Navbar.jsx";
 import { BrowserRouter } from "react-router-dom";
+import Styles from "./components/Home/Home.module.css"
 
 function App() {
   return (
     <BrowserRouter>
+      <div className={Styles.fundohome}>
       <Navbar />
+      </div>
     </BrowserRouter>
   );
 }
