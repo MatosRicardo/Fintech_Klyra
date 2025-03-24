02121826634e9de40e61ab5c0ba24ee01ef0eb4c
Paulo Henrique Nalcino <phnalcino@gmail.com>
2025-03-23 23:01:59 -0300

    home: insert text, aling items

--- klyra/src/App.jsx
+++ klyra/src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/navbar/Navbar.jsx";
 import { BrowserRouter } from "react-router-dom";
 import Styles from "./components/Home/Home.module.css"
+import Home from "./components/Home/Home.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <div className={Styles.fundohome}>
       <Navbar />
+      <Home />
       </div>
     </BrowserRouter>
   );
---
+++ klyra/src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Styles from'../Home/Home.module.css'
+
+
+function Home(){
+    return(
+        <div className={Styles.Texto}>
+            <h1>Invista com inteligencia,<br />
+            cresça com simplicidade.</h1>
+        </div>
+
+    )
+}
+
+export default Home
